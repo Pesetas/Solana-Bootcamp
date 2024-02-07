7a95fc194ec73634ac591c930e4d73c2d75c7901
Antonio Ramírez <aramirezmartin@hotmail.com>
2024-02-07 18:13:07 +0100

    Update polyfills.ts

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -1,5 +1,6 @@
-import {Buffer} from "buffer";
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { Buffer } from "buffer";
 
 (window as any).global = window;
 (window as any).global.Buffer = Buffer;
-(window as any).process = process;
+(window as any).process = { env: { }};  
